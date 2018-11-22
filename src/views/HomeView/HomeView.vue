c32cb9b51ccb8c7b5a2a9a6d3fce30524a6a9da9
linmei <1098188079@qq.com>
2018-11-22 16:07:36 +0800

    首页静态页面编写

--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -1,3 +1,6 @@
+<style lang="less">
+  @import "./HomeView";
+</style>
 <template>
   <div>
     <mt-header fixed title="小米家的店"></mt-header>
@@ -40,12 +43,14 @@
               <img class='hot-icon' src='../../assets/shopping-icon/burst_clothes/hot.png' />
               <h3 class='red-title'>{{item.redTitle}}</h3>
               <p :class="item.firstImg ? 'firstImgText':'black-title'">{{item.blackTitle}}</p>
-              <p v-if="item.firstImg" class="item.firstImg ? 'firstImgText':'black-title'">{{item.spanTitle}}</p>
-              <div :class="item.firstImg ? 'firstImg':'color-list'" v-if="!item.firstImg" >
+              <p v-if="item.firstImg" :class="item.firstImg ? 'firstImgText2':'black-title'">{{item.spanTitle}}</p>
+              <div :class="item.firstImg ? 'firstImg':'color-list'" >
                 <img class='small-img'  v-for="(imgItem,imgIndex) in borstList[index].colorList"  :key="'i'+imgIndex" :src='imgItem' />
               </div>
               <div class='clear'></div>
-              <button v-if="item.firstImg" class='buyBtnFirstImg'>点击购买</button>
+              <div class='positionbox'v-if="item.firstImg" >
+                <button class='buyBtnFirstImg'>点击购买</button>
+              </div>
               <div v-else class='positionbox'>
                 <span class='span-title'>{{item.spanTitle}}</span>
                 <button class='buyBtn'>点击购买</button>
@@ -55,7 +60,16 @@
         </Row>
       </Card >
       <div class='clear'></div>
+      <div class='needToBuyList'>
+        <p class='needToBuyTitle'>必买清单</p>
+        <ul class='toBuyImgList'>
+          <li v-for="(item,index) in toBuyImgList" :key="'imgID'+index">
+            <img v-lazy="item" :class="'imgClass'+index">
+          </li>
+        </ul>
+      </div>
     </div>
+    <!--<button>查看更多</button>-->
   </div>
 </template>
 <script>
@@ -82,235 +96,151 @@
         classifyList:[
           {
             clothesName:'羽绒服',
-            clothesImg:'/assets/shopping-icon/clothes01.png'
+            clothesImg: require('../../assets/shopping-icon/clothes01.png')
           },
           {
             clothesName: '棉服',
-            clothesImg: '../assets/shopping-icon/clothes02.png'
+            clothesImg: require('../../assets/shopping-icon/clothes02.png')
           },
           {
             clothesName: '套装',
-            clothesImg: '@/assets/shopping-icon/clothes03.png'
+            clothesImg: require('../../assets/shopping-icon/clothes03.png')
           },
           {
             clothesName: '卫衣',
-            clothesImg: '../../../assets/shopping-icon/clothes04.png'
+            clothesImg: require('../../assets/shopping-icon/clothes04.png')
           },
           {
             clothesName: '外套',
-            clothesImg: '../../assets/shopping-icon/clothes05.png'
+            clothesImg: require('../../assets/shopping-icon/clothes05.png')
           },
           {
             clothesName: '毛衣',
-            clothesImg: '../../assets/shopping-icon/clothes06.png'
+            clothesImg: require('../../assets/shopping-icon/clothes06.png')
           },
           {
             clothesName: '打底衫',
-            clothesImg: '../../assets/shopping-icon/clothes07.png'
+            clothesImg: require('../../assets/shopping-icon/clothes07.png')
           },
           {
             clothesName: '裤子',
-            clothesImg: '../../../src/assets/shopping-icon/clothes08.png'
+            clothesImg: require('../../assets/shopping-icon/clothes08.png')
           },
         ],
         borstList:[
           {
             id:'1',
-            bigImg:'../../../assets/shopping-icon/burst_clothes/burst_big01.png',
+            bigImg:require("../../assets/shopping-icon/burst_clothes/burst_big01.png"),
             redTitle:'爆款纯棉多色内衣套装',
             blackTitle:'累计售出164000+件',
             spanTitle:'五色可选',
             colorList:[
-              '../../../assets/shopping-icon/burst_clothes/burst1_small01.png',
-              '@/assets/shopping-icon/burst_clothes/burst1_small02.png',
-              '/assets/shopping-icon/burst_clothes/burst1_small03.png',
-              '/assets/shopping-icon/burst_clothes/burst1_small04.png',
-              '/assets/shopping-icon/burst_clothes/burst1_small05.png'
+              require('../../assets/shopping-icon/burst_clothes/burst1_small01.png'),
+              require('../../assets/shopping-icon/burst_clothes/burst1_small02.png'),
+              require('../../assets/shopping-icon/burst_clothes/burst1_small03.png'),
+              require('../../assets/shopping-icon/burst_clothes/burst1_small04.png'),
+              require('../../assets/shopping-icon/burst_clothes/burst1_small05.png')
             ],
             firstImg: false,
           },
           {
             id: '2',
-            bigImg: '/assets/shopping-icon/burst_clothes/burst_big02.png',
+            bigImg: require('../../assets/shopping-icon/burst_clothes/burst_big02.png'),
             redTitle: '热销款金丝绒保暖套装',
             blackTitle: '累计售出6600+件',
             spanTitle: '五款可选',
             colorList: [
-              '/assets/shopping-icon/burst_clothes/burst2_small01.png',
-              '/assets/shopping-icon/burst_clothes/burst2_small02.png',
-              '/assets/shopping-icon/burst_clothes/burst2_small03.png',
-              '/assets/shopping-icon/burst_clothes/burst2_small04.png',
-              '/assets/shopping-icon/burst_clothes/burst2_small05.png'
+              require('../../assets/shopping-icon/burst_clothes/burst2_small01.png'),
+              require('../../assets/shopping-icon/burst_clothes/burst2_small02.png'),
+              require('../../assets/shopping-icon/burst_clothes/burst2_small03.png'),
+              require('../../assets/shopping-icon/burst_clothes/burst2_small04.png'),
+              require('../../assets/shopping-icon/burst_clothes/burst2_small05.png')
             ],
             firstImg: false,
           },
           {
             id: '3',
-            bigImg: '/assets/shopping-icon/burst_clothes/burst_big03.png',
+            bigImg: require('../../assets/shopping-icon/burst_clothes/burst_big03.png'),
             redTitle: '热销款加绒白搭皮裤',
             blackTitle: '累计售出2900+件',
             spanTitle: '三款可选',
             colorList: [
-              '/assets/shopping-icon/burst_clothes/burst3_small01.png',
-              '/assets/shopping-icon/burst_clothes/burst3_small02.png',
-              '/assets/shopping-icon/burst_clothes/burst3_small03.png'
+              require('../../assets/shopping-icon/burst_clothes/burst3_small01.png'),
+              require('../../assets/shopping-icon/burst_clothes/burst3_small02.png'),
+              require('../../assets/shopping-icon/burst_clothes/burst3_small03.png')
             ],
             firstImg: false,
           },
           {
             id: '4',
-            bigImg: '/assets/shopping-icon/burst_clothes/burst_big04.png',
+            bigImg: require('../../assets/shopping-icon/burst_clothes/burst_big04.png'),
             redTitle: '超值！背心保暖套装',
             blackTitle: '金丝绒500克加绒',
             spanTitle: '四色可选',
             colorList: [
-              '/assets/shopping-icon/burst_clothes/burst4_small01.png',
-              '/assets/shopping-icon/burst_clothes/burst4_small02.png',
-              '/assets/shopping-icon/burst_clothes/burst4_small03.png',
-              '/assets/shopping-icon/burst_clothes/burst4_small04.png'
+              require('../../assets/shopping-icon/burst_clothes/burst4_small01.png'),
+              require('../../assets/shopping-icon/burst_clothes/burst4_small02.png'),
+              require('../../assets/shopping-icon/burst_clothes/burst4_small03.png'),
+              require('../../assets/shopping-icon/burst_clothes/burst4_small04.png')
             ],
             firstImg: false,
           },
           {
             id: '5',
-            bigImg: '/assets/shopping-icon/burst_clothes/burst_big05.png',
+            bigImg: require('../../assets/shopping-icon/burst_clothes/burst_big05.png'),
             redTitle: '潮款百搭加绒休闲裤',
             blackTitle: '必备款 金丝绒加厚',
             spanTitle: '版型超正 热卖款',
             colorList: [
-              '/assets/shopping-icon/burst_clothes/burst5_small01.png'
+              require('../../assets/shopping-icon/burst_clothes/burst5_small01.png')
             ],
             firstImg:true,
           },
           {
             id: '6',
-            bigImg: '/assets/shopping-icon/burst_clothes/burst_big06.png',
+            bigImg: require('../../assets/shopping-icon/burst_clothes/burst_big06.png'),
             redTitle: '潮款百搭高领打底衫',
             blackTitle: '400克厚棉单层',
             spanTitle: '四色可选',
             colorList: [
-              '/assets/shopping-icon/burst_clothes/burst6_small01.png',
-              '/assets/shopping-icon/burst_clothes/burst6_small02.png',
-              '/assets/shopping-icon/burst_clothes/burst6_small03.png',
-              '/assets/shopping-icon/burst_clothes/burst6_small04.png'
+              require('../../assets/shopping-icon/burst_clothes/burst6_small01.png'),
+              require('../../assets/shopping-icon/burst_clothes/burst6_small02.png'),
+              require('../../assets/shopping-icon/burst_clothes/burst6_small03.png'),
+              require('../../assets/shopping-icon/burst_clothes/burst6_small04.png')
             ],
             firstImg: false,
           }
         ],
+        toBuyImgList:[
+          require('../../assets/shopping-icon/need_buy_clothes/need01.png'),
+          require('../../assets/shopping-icon/need_buy_clothes/need02.png'),
+          require('../../assets/shopping-icon/need_buy_clothes/need03.png'),
+          require('../../assets/shopping-icon/need_buy_clothes/need04.png'),
+          require('../../assets/shopping-icon/need_buy_clothes/need05.png'),
+          require('../../assets/shopping-icon/need_buy_clothes/need06.png'),
+          require('../../assets/shopping-icon/need_buy_clothes/need07.png'),
+          require('../../assets/shopping-icon/need_buy_clothes/need08.png'),
+          require('../../assets/shopping-icon/need_buy_clothes/need09.png'),
+          require('../../assets/shopping-icon/need_buy_clothes/need10.png'),
+          require('../../assets/shopping-icon/need_buy_clothes/need11.png'),
+          require('../../assets/shopping-icon/need_buy_clothes/need12.png'),
+          require('../../assets/shopping-icon/need_buy_clothes/need13.png'),
+          require('../../assets/shopping-icon/need_buy_clothes/need14.png'),
+          require('../../assets/shopping-icon/need_buy_clothes/need15.png'),
+          require('../../assets/shopping-icon/need_buy_clothes/need16.png'),
+          require('../../assets/shopping-icon/need_buy_clothes/need17.png'),
+        ],
+
       }
+    },
+    methods:{
+      init(){
+
+      }
+    },
+    mounted(){
+      this.init();
     }
   }
 </script>
-<style lang="less">
-  .mint-header{
-    background-color:#d4237a;
-  }
-  .home_conter{
-    margin-top:40px;
-    width:100%;
-    height:200px;
-  }
-  .swiper_img{
-    width:100%;
-    height:200px;
-    display:inline-block;
-  }
-  /*优惠折扣*/
-  .coupon{
-    padding:10px 30px;
-    .coupon__title{
-      font-size:16px;
-      font-weight:bold;
-      color:orange;
-      line-height:40px;
-    }
-    .coupon-item{
-      background:orange;
-      padding:10px;
-      margin-right:1px;
-      text-align:center;
-      .cou-sign{
-        text-align:left;
-        display:block;
-      }
-      .cou-money{
-        font-size:36px;
-      }
-      .cou-use-method{
-        font-size:10px;
-        display:block;
-      }
-      .cou-time{
-        font-size:8px;
-        background:#000;
-        color:#fff;
-        width:100%;
-        display:block;
-        line-height:12px;
-      }
-    }
-  }
-  /*热门分类*/
-  .HOT-classify{
-    text-align:center;
-    .Hot-title{
-      font-size:18px;
-      font-weight:bold;
-    }
-    .clothes-classify {
-      width:282px;
-      height:200px;
-      margin:10px auto;
-      .classify-list{
-        width:70px;
-        height:90px;
-        text-align:center;
-        border:1px solid #ccc;
-        float:left;
-        list-style:none;
-        .classify-item{
-          font-size:12px;
-          margin:10px;
-        }
-        .clothes-img{
-          width:40px;
-          height:40px;
-        }
-      }
-    }
-  }
-  /*推荐款*/
-  .Bursting-clothes{
-    .burst-title{
-      font-size:24px;
-      color:red;
-    }
-    .burst-view{
-      height:140px;
-      border:1px solid red;
-      border-radius:3px;
-      padding:10px;
-      margin:10px;
-      .big-img{
-        width:100%;
-        height:100px;
-        display:block;
-      }
-      .textExplain{
-        margin:10px;
-        padding-top:10px;
-        position:relative;
-        .hot-icon{
-          position:absolute;
-          top:-10px;
-          right:-10px;
-          width:20px;
-          height:20px;
-          display:block;
-        }
-      }
 
-    }
-  }
-
-</style>
