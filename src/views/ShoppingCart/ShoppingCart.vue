6e4497499c4536c5a7a7b5ed2301a13441912679
linmei <1098188079@qq.com>
2018-11-27 18:05:41 +0800

    购物车单选全选全不选

--- src/views/ShoppingCart/ShoppingCart.vue
+++ src/views/ShoppingCart/ShoppingCart.vue
@@ -5,57 +5,56 @@
   <div>
     <mt-header fixed title="购物车"></mt-header>
     <div class="shop_header">
-      <Checkbox class="checkAllShop"
-        :indeterminate="indeterminate"
-        :value="checkAll"
-        @click.prevent.native="handleCheckAll">
+      <Checkbox type='checkbox' class="checkAllShop"
+                :value="isCheckedAll"
+                @on-change="handleCheckAll()"></Checkbox>
         <img src="../../assets/other_icon/shop.png" style="width:16px;height:16px;margin-left:10px;" />
         小米家的店
-      </Checkbox>
       <a class="shop_cart_edit" v-if="!isEdit" @click="shopCartEdit">编辑</a>
       <a class="shop_cart_edit" v-else @click="shopCartFinish">完成</a>
     </div>
     <div class="shop_content">
-      <CheckboxGroup v-model="checkAllGroup" @on-change="checkAllGroupChange">
-          <ul>
-            <li class="shop_cart_list">
-              <Checkbox  label=""></Checkbox>
-              <div class="shop_cart_item">
-                <Col span="8">
-                  <img src="../../assets/shopping-icon/need_buy_clothes/need01.png" />
-                </Col>
-                <Col span="16" class="text_conter">
-                  <div v-if="isEdit" class="nub_roughly">
-                    <span @click="mimusBtn">-</span>
-                    <span class="shop_nub">3</span>
-                    <span @click="addBtn">+</span>
-                  </div>
-                  <p v-else>商品介绍j可分为几个配角安排给附加额外加分</p>
-                  <span class="text_remark">备注分为嘎Greg经理</span>
-                  <div class="money_nub">
-                    <span class="money_text">￥69.00</span>
-                    <span class="nub_text">x1</span>
-                  </div>
-                </Col>
-              </div>
-              <div v-if="isEdit" class="remove-btn">
-                删除
-              </div>
-            </li>
-          </ul>
-      </CheckboxGroup>
+        <ul>
+          <li class="shop_cart_list" v-for="item in shopCartList" :key="item.shopID">
+            <Checkbox type='checkbox' :value="item.checked" @on-change='checkedOne(item,item.shopID)'></Checkbox>
+            <div class="shop_cart_item">
+              <Col span="8">
+                <img :src="item.img" />
+              </Col>
+              <Col span="16" class="text_conter">
+                <div v-if="isEdit" class="nub_roughly">
+                  <span @click="changeMoney(item,-1)">-</span>
+                  <span class="shop_nub">{{ item.shopNub }}</span>
+                  <span @click="changeMoney(item,1)">+</span>
+                </div>
+                <p v-else>{{ item.shopTitle }}</p>
+                <span class="text_remark">{{ item.shopRemark }}</span>
+                <div class="money_nub">
+                  <span class="money_text">{{ item.unitPrice | formatMoney }}</span>
+                  <span class="nub_text">x1</span>
+                </div>
+              </Col>
+            </div>
+            <div v-if="isEdit" class="remove-btn">
+              删除
+            </div>
+
+          </li>
+        </ul>
+
     </div>
     <div class="shop_footer">
       <Checkbox class="checkAllShop"
-                :indeterminate="indeterminate"
-                :value="checkAll">全选</Checkbox>
+                @on-change="handleCheckAll"
+                :value="isCheckedAll">全选</Checkbox>
       <div class="settlement_content">
         <Col span="16" class="settlement_text">
-          <p>合计：<span style="color:red;">￥69.00</span></p>
+          <p>合计：<span style="color:red;">{{ TotalSum | formatMoney }}</span></p>
           <span style="font-size:10px;color:grey;">不含运费</span>
         </Col>
         <Col span="8">
-          <button class="settlement_btn">结算</button>
+          <button v-if="!isEdit" :class="isChecked?'redSettlement_btn':'settlement_btn'">结算</button>
+          <button v-else class="settlement_btn allRemove_btn">删除</button>
         </Col>
       </div>
     </div>
@@ -66,37 +65,81 @@
     data() {
       return {
         indeterminate: true,
-        checkAll: false,
-        checkAllGroup: [],
-        isEdit: false,
+        checkAll: false,  //全选
+        checkAllGroup: [], //选中的商品
+        isEdit: false,  //是否编辑
+        isCheckedAll:false, //是否全选
+        selectedShopArr:[], //选择的商品
+        shopCartList:[   //购物车列表数据
+          {
+            shopID: 1,
+            img: require('../../assets/shopping-icon/need_buy_clothes/need01.png'),
+            shopNub: 1,    //数量
+            shopTitle:'商品介绍j可分为几个配角安排给附加额外加分',
+            shopRemark:'备注分为嘎Greg经理',
+            unitPrice: 69,  //单价
+            checked:false
+          },
+          {
+            shopID: 2,
+            img: require('../../assets/shopping-icon/need_buy_clothes/need01.png'),
+            shopNub: 1,    //数量
+            shopTitle:'商品介绍j可分为几个配角安排给附加额外加分',
+            shopRemark:'备注分为嘎Greg经理',
+            unitPrice: 69,  //单价
+            checked:false
+          },
+          {
+            shopID: 3,
+            img: require('../../assets/shopping-icon/need_buy_clothes/need01.png'),
+            shopNub: 1,    //数量
+            shopTitle:'商品介绍j可分为几个配角安排给附加额外加分',
+            shopRemark:'备注分为嘎Greg经理',
+            unitPrice: 69,  //单价
+            checked:false
+          },
+          {
+            shopID: 4,
+            img: require('../../assets/shopping-icon/need_buy_clothes/need01.png'),
+            shopNub: 1,    //数量
+            shopTitle:'商品介绍j可分为几个配角安排给附加额外加分',
+            shopRemark:'备注分为嘎Greg经理',
+            unitPrice: 69,  //单价
+            checked:false
+          },
+        ],
+        TotalSum:'',  //合计金额
+        isChecked:false, //是否选中
+      }
+    },
+    filters:{
+      formatMoney(value){
+        value = Number(value);
+        return "￥"+ value.toFixed(2);
       }
     },
     methods: {
+      //全选
       handleCheckAll () {
-        if (this.indeterminate) {
-          this.checkAll = false;
-        } else {
-          this.checkAll = !this.checkAll;
-        }
-        this.indeterminate = false;
-
-        if (this.checkAll) {
-          this.checkAllGroup = ['1'];
-        } else {
-          this.checkAllGroup = [];
-        }
+        this.isCheckedAll = !this.isCheckedAll;
+        let selected = this.isCheckedAll;
+        this.selectedShopArr = [];
+        this.shopCartList.forEach((item)=>{
+          item.checked = selected;
+          this.selectedShopArr.push(item.checked);
+        });
+        this.isChecked = selected;
       },
-      checkAllGroupChange (data) {
-        if (data.length === 3) {
-          this.indeterminate = false;
-          this.checkAll = true;
-        } else if (data.length > 0) {
-          this.indeterminate = true;
-          this.checkAll = false;
-        } else {
-          this.indeterminate = false;
-          this.checkAll = false;
+      //单选
+      checkedOne(item,index){
+        item.checked =!item.checked;
+        let selected = item.checked
+        this.selectedShopArr.splice(index-1, 1, selected);
+        //判断商品是否全选
+        if(this.selectedShopArr.length === this.shopCartList.length){
+          this.isCheckedAll = !this.selectedShopArr.includes(false);
         }
+        this.isChecked = selected;
       },
     //  编辑
       shopCartEdit(){
@@ -106,15 +149,27 @@
       shopCartFinish(){
         this.isEdit = false;
       },
-    // 减
-      mimusBtn(){
-
+    // 加减
+      changeMoney(product,type){
+        if(type > 0){
+          product.shopNub++;
+        }else{
+          product.shopNub--;
+          if(product.shopNub<1){
+            product.shopNub = 1;
+          }
+        }
       },
-      //加
-      addBtn(){
+    },
+    mounted(){
+      this.shopCartList.forEach((item)=>{
+        this.TotalSum = item.shopNub*item.unitPrice;
 
-      }
-    }
+      })
+
+
+    },
+
   }
 </script>
 
